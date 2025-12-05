884472dfe4ba60de74e0a037feeb739f58a11c07
aleksmeshr <aleksandr@ilert.com>
2025-12-05 11:41:57 +0100

    remove debug logs

--- rig-core/src/providers/mistral/completion.rs
+++ rig-core/src/providers/mistral/completion.rs
@@ -115,8 +115,6 @@ impl TryFrom<message::Message> for Vec<Message> {
     fn try_from(message: message::Message) -> Result<Self, Self::Error> {
         match message {
             message::Message::User { content } => {
-                println!("rig messages: {:?}", content);
-
                 let mut tool_result_messages = Vec::new();
                 let mut other_messages = Vec::new();
 
@@ -162,7 +160,6 @@ impl TryFrom<message::Message> for Vec<Message> {
                 }
 
                 tool_result_messages.append(&mut other_messages);
-                println!("mistral messages: {:?}", tool_result_messages);
                 Ok(tool_result_messages)
             }
             message::Message::Assistant { content, .. } => {
@@ -382,9 +379,6 @@ impl TryFrom<(&str, CompletionRequest)> for MistralCompletionRequest {
             .clone()
             .map(crate::providers::openai::completion::ToolChoice::try_from)
             .transpose()?;
-
-        println!("History in rig format: {:?}", req.chat_history);
-        println!("History in mistral format: {:?}", full_history);
 
         Ok(Self {
             model: model.to_string(),
